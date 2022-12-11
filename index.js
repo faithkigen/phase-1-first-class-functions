389af7dffa742f1bffb2f001e10c330c18ef9975
Faith <kigenfaith20@gmail.com>
2022-12-11 19:58:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const returnFirstTwoDrivers = function (drivers) {
-    const firstTwoDrivers = drivers.slice(0, 2);
-    return firstTwoDrivers;
-  };
-  
-  const returnLastTwoDrivers = function (drivers) {
-    const lastTwoDrivers = drivers.slice(-2);
-    return lastTwoDrivers;
-  };
-  
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  const createFareMultiplier = function (multiplier) {
-    return function (value) {
-      return value * multiplier;
-    };
-  };
-  
-  const fareDoubler = createFareMultiplier(2);
-  
-  const fareTripler = createFareMultiplier(3);
-  
-  const selectDifferentDrivers = function (drivers) {
-    return fn(drivers);
-  };
+
+function receivesAFunction(callback){
+  callback();
+}
+receivesAFunction(() => {
+
+})
+function returnsANamedFunction(){
+  let faith= () => console.log("name")
+  return (faith)
+}
+var returnsAnAnonymousFunction = function() {
+  return (() => {
+      alert('This is an anonymous function')
+  }
+  )
+}
